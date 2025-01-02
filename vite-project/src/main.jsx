a66717964b943f05190eb4ca8e98242e66108aa2
VICTORIA MARTINS <TORIMARTINS27@GMAIL.COM>
2025-01-02 14:40:43 -0500

    Basic header structure complete

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
