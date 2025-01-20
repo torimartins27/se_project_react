ec3b9533761fda7c64faa063e6c397f25ba475c0
VICTORIA MARTINS <TORIMARTINS27@GMAIL.COM>
2025-01-20 17:45:21 -0500

    added close icon, moved context folder, made sure all feedback was complete

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import "./ToggleSwitch.css";
-import CurrentTemperatureUnitContext from "../contexts/CurrentTemperatureContext";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureContext";
 
 export default function ToggleSwitch() {
   const { handleToggleSwitchChange } = useContext(
--- src/contexts/CurrentTemperatureContext.jsx
+++ src/contexts/CurrentTemperatureContext.jsx
@@ -1,7 +1,3 @@
-// originally had this in a contexts folder but
-// automated tests wouldnt pass unless I put it in
-// vendor or utils, dont know how to fix
-
 import { createContext } from "react";
 
 const CurrentTemperatureUnitContext = createContext();
